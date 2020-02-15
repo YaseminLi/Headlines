8ae04c5ef26c46e5918ec35d1c6af19ca9072161
YaseminLi <yaseminli@163.com>
2020-02-15 23:52:50 +0800

    样式美化

--- src/js/component/pc_news_container.js
+++ src/js/component/pc_news_container.js
@@ -1,6 +1,6 @@
 //news共用的部分
 import React from 'react';
-import { Carousel, Row, Col, Tabs } from 'antd';
+import { Carousel, Col, Tabs, List, Avatar, Icon } from 'antd';
 const TabPane = Tabs.TabPane;
 import PCNewsClock from './pc_news_block';
 import PCNewsImageBlock from './pc_news_image_block';
@@ -16,56 +16,80 @@ export default class PCNewsContainer extends React.Component {
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true
-        }; 
-        const guojiNews=newsData.guoji
-        const topNews=newsData.top
-        const guoneiNews=newsData.guonei
-        const yuleNews=newsData.yule
+        };
+        const guojiNews = newsData.guoji
+        const topNews = newsData.top
+        const guoneiNews = newsData.guonei
+        const yuleNews = newsData.yule
         return (
             <div>
-                <Row>
-                    <Col span={2}></Col>
-                    <Col span={20} className='container'>
-                        <div className='leftContainer'>
-                            <div className='carousel' >
-                                <Carousel {...settings}>
-                                    <div><img src='./src/images/carousel_1.jpg' /></div>
-                                    <div><img src='./src/images/carousel_2.jpg' /></div>
-                                    <div><img src='./src/images/carousel_3.jpg' /></div>
-                                    <div><img src='./src/images/carousel_4.jpg' /></div>
-                                </Carousel>
+                <Col span={2}></Col>
+                <Col span={20} className='container'>
+                    <div className='leftContainer'>
+                        <div className='carousel' >
+                            <Carousel {...settings}>
+                                <div><img src='./src/images/carousel_1.jpg' /></div>
+                                <div><img src='./src/images/carousel_2.jpg' /></div>
+                                <div><img src='./src/images/carousel_3.jpg' /></div>
+                                <div><img src='./src/images/carousel_4.jpg' /></div>
+                            </Carousel>
+                        </div>
+                        {/* <PCNewsImageBlock count='6' type='guoji' cardtitle='国际新闻' cardwidth='112px' news={guojiNews} /> */}
+                        <List
+                            itemLayout="vertical"
+                            size="large"
+                            dataSource={guojiNews.slice(0,6)}
+                            renderItem={item => (
+                                <List.Item
+                                    key={item.title} 
+                                    extra={
+                                        <img
+                                            width={200}
+                                            alt="logo"
+                                            src={item.thumbnail_pic_s}
+                                        />
+                                    }
+                                >
+                                    {/* <List.Item.Meta
+                                        avatar={<Avatar src={item.avatar} />}
+                                        title={<a href={item.href}>{item.title}</a>}
+                                        description={item.description}
+                                    /> */}
+                                    {item.title}
+                                </List.Item>
+                            )}
+                        />
+                    </div>
+                    <div className='centerContainer'>
+                        <div className='top'>
+                            <div className='tabs_news'>
+                                <div className='header'>
+                                    <p className='chinese'>今日推荐</p><p className='En'>HOT</p>
+                                </div>
+                                <List
+                                    bordered={false}
+                                    split={false}
+                                    dataSource={topNews.slice(0, 6)}
+                                    renderItem={item => (
+                                        <List.Item>
+                                            {/* <Typography.Text mark>[ITEM]</Typography.Text>  */}
+                                            {item.title}
+                                        </List.Item>
+                                    )}
+                                />
                             </div>
-                            <PCNewsImageBlock count='6' type='guoji' cardtitle='国际新闻' cardwidth='112px' news={guojiNews}/>
+                            <Tabs className='pc_prodcuct'>
+                                <TabPane tab='媒体合作' key='1'>
+                                    <PCProduct />
+                                </TabPane>
+                            </Tabs>
                         </div>
-                        <Tabs className='tabs_news' style={{ width: 600 }}>
-                            <TabPane tab='头条新闻' key='1'>
-                                <PCNewsClock count='25' type='top' bordered="false" news={topNews}/>
-                            </TabPane>
-                            <TabPane tab='娱乐新闻' key='2'>
-                                <PCNewsClock count='27' type='yule' width="400px" bordered="false" news={yuleNews}/>
-                            </TabPane>
-                        </Tabs>
-                        <Tabs className='pc_prodcuct'>
-                            <TabPane tab='媒体合作' key='1'>
-                                <PCProduct />
-                            </TabPane>
-                        </Tabs>
-                    </Col>
-                    <Col span={2}></Col>
-                </Row>
-                <Row>
-                    <Col span={2}></Col>
-                    <Col span={20}>
-                        <div>
-                            <PCNewsImageBlock count='9' type='guonei' cardtitle='国内新闻' width='100%' cardwidth='112px' news={guoneiNews}/>
-                            <PCNewsImageBlock count='18' type='yule' cardtitle='娱乐新闻' width='100%' cardwidth='112px' news={yuleNews}/>
-                        </div>
-                    </Col>
-                    <Col span={2}></Col>
-                </Row>
-
+                        <PCNewsImageBlock count='9' type='guonei' cardtitle='国内新闻' width='100%' cardwidth='112px' news={guoneiNews} />
+                        <PCNewsImageBlock count='18' type='yule' cardtitle='娱乐新闻' width='100%' cardwidth='112px' news={yuleNews} />
+                    </div>
+                </Col>
+                <Col span={2}></Col>
             </div>
-
         )
     }
 }
